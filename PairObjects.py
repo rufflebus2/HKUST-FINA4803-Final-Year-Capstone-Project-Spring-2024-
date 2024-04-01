a758f76782272571f22ffbd336d728d80aed97a8
rufflebus2 <165698576+rufflebus2@users.noreply.github.com>
2024-04-02 01:13:30 +0800

    Removed extra comments

--- PairObjects.py
+++ PairObjects.py
@@ -38,13 +38,8 @@ class Pairs(object):
         df = pd.concat([self.a.DataFrame.droplevel([0]), self.b.DataFrame.droplevel([0])], axis=1).dropna()
         df.columns = [self.a.Symbol.Value, self.b.Symbol.Value]
         return df
-    
-    # @property
-    # def Correlation(self):
-    #     return self.DataFrame.corr().iloc[0][1]
 
     def resetParams(self, algorithm):
-        #algorithm.Debug(f'Params reset for {self.a.symbol} and {self.b.symbol}')
         # Cointegration p-value
         self.coint_test = -1
 
@@ -66,16 +61,7 @@ class Pairs(object):
         self.ticket_b = None
 
     def cointegration_test(self, algorithm, price_a, price_b):
-        # index [1] is for the cointegration p-value
-        if not price_a.index.equals(price_b.index):
-            algorithm.Debug(f'{self.a.symbol} and {self.b.symbol} Invalid indexes')
-            return False
-
         self.coint_test = coint(price_a, price_b, trend="n", maxlag=0)[1]
         if self.coint_test >= 0.05:
             return False
         return True
-
-    # def __str__(self):
-    #     # print out pair name, correlation and cointegration p-value
-    #     return f"{self.Name} \nCorrelation: {round(self.Correlation, 3)} \nCointegration: {round(self.coint_test, 3)}"
